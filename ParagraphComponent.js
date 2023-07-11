2311b32f4bea19d332953eb48fb4e9737d4ac85d
glooms00 <doha@CTD.ma>
2023-07-11 11:56:00 +0100

    new project

--- ParagraphComponent.js
+++ ParagraphComponent.js
@@ -5,14 +5,12 @@ import { View, Text } from 'react-native';
 import styles from './styles';
 
 const ParagraphComponent = ({ paragraphTextData }) => {
-  const { newText, paragraphText1, paragraphText2, paragraphText3 } = paragraphTextData;
+  const { newText, paragraphs } = paragraphTextData;
 
   return (
     <View>
       <Text style={styles.newText}>{newText}</Text>
-      <Text style={styles.p}>{paragraphText1}</Text>
-      <Text style={styles.p}>{paragraphText2}</Text>
-      <Text style={styles.p}>{paragraphText3}</Text>
+      <Text style={styles.p}>{paragraphs.join('\n\n')}</Text>
     </View>
   );
 };
--- data.js
+++ data.js
@@ -6,8 +6,9 @@ export const imageTextData = {
   
   export const paragraphTextData = {
     newText: "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant",
-    paragraphText1: "du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500",
-    paragraphText2: "Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression",
-    paragraphText3: "Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression",
-  };
-  
+    paragraphs: [
+        "du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500",
+        "Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression",
+        "Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression"
+      ],
+ };
